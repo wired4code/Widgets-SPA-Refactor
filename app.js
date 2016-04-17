752ae09e9c848eeede9c72f34f8ab5c27dc60f21
Sonny Sheth <sonny.sheth@gmail.com>
2016-04-17 15:22:31 -0300

    Add script links, set up template routes

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
+app.config('$routeProvider', $locationProvider, [function($routeProvider, locationProvider){
+
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'home.html',
+      controller: 'homeController'
+    })
+
+    .when('/user', {
+      templateUrl: 'user.html',
+      controller: 'userController'
+    })
+
+    .when('/widget', {
+      templateUrl: 'widget.html',
+      controller: 'widgetController'
+    })
+
+
+  $locationProvider.html5Mode(true);
+
+}])
